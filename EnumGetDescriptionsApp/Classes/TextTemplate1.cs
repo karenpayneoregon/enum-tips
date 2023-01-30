81a406133ac7ae5e54e969b55f815b2a368f3015
Karen Payne <paynekaren@comcast.net>
2023-01-30 10:29:00 -0800

    experimenting

--- EnumGetDescriptionsApp/Classes/TextTemplate1.cs
+++ EnumGetDescriptionsApp/Classes/TextTemplate1.cs
@@ -1,11 +1,10 @@
-﻿
-
-
- 
-
-public static class MakeSelect { public const string Value = "MakeSelect";public const string Text = "Select";}
-public static class SpaceTravel { public const string Value = "SpaceTravel";public const string Text = "Space Travel";}
-public static class Adventure { public const string Value = "Adventure";public const string Text = "Adventure";}
-public static class Sports { public const string Value = "Sports";public const string Text = "Popular sports";}
-public static class Automobile { public const string Value = "Automobile";public const string Text = "Cars";}
-public static class Programming { public const string Value = "Programming";public const string Text = "Programming with C#";}
+﻿ 
+namespace EnumGetDescriptionsApp.Classes
+{
+    public static class MakeSelect { public const string Value = "MakeSelect";public const string Text = "Select";}
+    public static class SpaceTravel { public const string Value = "SpaceTravel";public const string Text = "Space Travel";}
+    public static class Adventure { public const string Value = "Adventure";public const string Text = "Adventure";}
+    public static class Sports { public const string Value = "Sports";public const string Text = "Popular sports";}
+    public static class Automobile { public const string Value = "Automobile";public const string Text = "Cars";}
+    public static class Programming { public const string Value = "Programming";public const string Text = "Programming with C#";}
+} 
